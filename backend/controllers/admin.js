8da4ceb724e48d78b9e2d459d20cf1f409e40ff5
Usagi_Kazma <juan@DESKTOP-DILVCG8.localdomain>
2020-08-14 14:31:54 -0400

    Created all the Routes

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,3 +1,51 @@
+const Books = require("../models/Books");
+//Import the Books Model
+
 exports.getIndex = (req, res) => {
-    res.send('Book Library Home Route');
+  res.send("Book Library Home Route");
 };
+//Create A Route to Get the Home index of the API
+
+exports.getBooks = (req, res) => {
+  Books.fetchAll((books) => {
+    console.log(books);
+    res.json(books);
+  });
+};
+//Create GET Request to retrieve ALL the books
+
+exports.getBook = (req, res) => {
+  const bookId = req.params.bookId;
+
+  Books.findById(bookId, (book) => {
+    console.log(book);
+    res.json(book);
+  });
+};
+//Create GET request to look for book by ID
+
+exports.postAddBook = (req, res) => {
+  const {
+    name,
+    author,
+    narrated,
+    img,
+    bookLength,
+    releaseDate,
+    language,
+  } = req.body;
+
+  const book = new Books(
+    null,
+    name,
+    author,
+    narrated,
+    img,
+    bookLength,
+    releaseDate,
+    language
+  );
+  book.save();
+  res.json({ msg: "Book Added" });
+};
+//Create POST request to create a new instance of a Book.
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const adminController = require('../controllers/admin');
+const adminController = require("../controllers/admin");
 
-router.get('/', adminController.getIndex);
+router.get("/", adminController.getIndex);
+
+router.get("/books", adminController.getBooks);
+
+router.get("/books/:bookId", adminController.getBook);
+
+router.post("/add-book", adminController.postAddBook);
 
 module.exports = router;
